560926aa3c922571fad553c8af71c05ab5c5a46a
Rachmatuz <161696518+Rachmatuz@users.noreply.github.com>
2024-10-04 09:10:23 +0700

    -
    -

---
+++ tugas3.js
@@ -0,0 +1,10 @@
+/*function
+function penjumlahan(b,c) {
+    return jumlah = b + c;
+}
+console.log(penjumlahan(10,9));
+*/
+
+//arrow function
+const input = (d,z) => d + z;
+console.log(input(10,9));
