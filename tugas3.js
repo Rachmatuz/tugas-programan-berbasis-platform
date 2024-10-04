659e5abe309cedd5c54b8a178fa72aa8243441bb
Rachmatuz <161696518+Rachmatuz@users.noreply.github.com>
2024-10-04 09:31:50 +0700

    Update tugas3.js

--- tugas3.js
+++ tugas3.js
@@ -1,10 +1,15 @@
-/*function
+//function
 function penjumlahan(b,c) {
     return jumlah = b + c;
 }
 console.log(penjumlahan(10,9));
-*/
+
 
 //arrow function
 const input = (d,z) => d + z;
 console.log(input(10,9));
+
+
+//perbedaan
+//function menggunakan kata kunci function memiliki sintaksis panjang.
+//arrow function fungsi yang sama namun lebih ringkas dan lebih banyak operator ternary.
